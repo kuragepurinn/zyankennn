e0ca73fa039ac65809cdba3d27993d192f51a23d
kuragepurinn <67031827+kuragepurinn@users.noreply.github.com>
2020-06-24 12:02:45 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,38 +1,44 @@
 radio.onReceivedNumber(function (receivedNumber) {
     let 相手の手２ = 0
     let 相手の手１ = 0
-    let 自分の手 = false
-    if (自分の手 == (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Triangle)
-    } else if (自分の手 == (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Triangle)
-    } else if (自分の手 == (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Triangle)
-    } else if (自分の手 == (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Triangle)
-    } else if (自分の手 < (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.No)
-    } else if (自分の手 < (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.No)
-    } else if (自分の手 < (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.No)
-    } else if (自分の手 > (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Heart)
-    } else if (自分の手 > (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Heart)
-    } else if (自分の手 > (相手の手１ == 相手の手２)) {
-        basic.showIcon(IconNames.Heart)
+    basic.pause(2000)
+    if (相手の手１ == 1 && 相手の手２ == 1) {
+        if (自分の手 == 1) {
+            basic.showIcon(IconNames.Triangle)
+        } else if (自分の手 == 3) {
+            basic.showIcon(IconNames.Heart)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    } else if (相手の手１ == 2 && 相手の手２ == 2) {
+        if (自分の手 == 1) {
+            basic.showIcon(IconNames.Heart)
+        } else if (自分の手 == 2) {
+            basic.showIcon(IconNames.Triangle)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
+    } else if (相手の手１ == 3 && 相手の手２ == 3) {
+        if (自分の手 == 2) {
+            basic.showIcon(IconNames.Heart)
+        } else if (自分の手 == 3) {
+            basic.showIcon(IconNames.Triangle)
+        } else {
+            basic.showIcon(IconNames.No)
+        }
     } else {
-    	
+        basic.showIcon(IconNames.Triangle)
     }
 })
 input.onButtonPressed(Button.A, function () {
     basic.showIcon(IconNames.Scissors)
     radio.sendNumber(2)
+    自分の手 = 2
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(IconNames.Square)
     radio.sendNumber(3)
+    自分の手 = 3
 })
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
@@ -43,6 +49,8 @@ input.onButtonPressed(Button.B, function () {
         . . . . .
         `)
     radio.sendNumber(1)
+    自分の手 = 1
 })
+let 自分の手 = 0
 basic.showIcon(IconNames.Yes)
 radio.setGroup(8)
